4c4e2029d0802bfdce2b7647f3704b0367668456
Suprim24 <suprimachary88@gmail.com>
2025-07-02 16:24:37 +0545

    updated stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-const App = ()=>
-{
-  return(
+import { useState } from "react"
 
-    <>
-   <h1>Hi</h1>
-   <h2>Hello</h2>
-   <h3>hi</h3>
-    </>
+export const App = () => {
+    const [fruit, setfruit] = useState("Apple");
+    const fruitHndel = ()=>
+    {
+      setfruit("Banana")
+    }
+  return (
+  
+    <div>
+      <h1>state</h1>
+      <h1>{fruit}</h1>
+      <button onClick={fruitHndel}>change fruit</button>
+    </div>
   )
 }
-export default App
+const Practice =()=>
+{
+  
+}
+
+
